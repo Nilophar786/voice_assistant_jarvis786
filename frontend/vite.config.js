402542e06fe8e10255049453c296e8fecdf8afa1
Nilophar786 <shaikhnilofar0027@gmail.com>
2026-02-08 16:31:34 +0530

    nilo

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,5 +4,19 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),tailwindcss()],
+  plugins: [react(), tailwindcss()],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['react', 'react-dom'],
+          ui: ['framer-motion', 'react-icons', 'lottie-react', '@tsparticles/react', '@tsparticles/preset-stars'],
+          router: ['react-router-dom'],
+          utils: ['axios', 'react-speech-recognition', 'wavesurfer.js', 'react-tooltip', 'react-bootstrap'],
+           animations: ['gsap'],
+        }
+      }
+    },
+    chunkSizeWarningLimit: 1000
+  }
 })
